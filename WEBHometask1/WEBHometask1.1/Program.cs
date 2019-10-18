bb9165e06a5fb3fbf1de8e4bb4342bf44c959437
Nikita <nikitabaranov200021@gmail.com>
2019-10-18 19:56:41 +1000

    Fix some troubles

---
+++ WEBHometask1/WEBHometask1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEBHometask1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
