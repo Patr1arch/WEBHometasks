521733b5e2b0c5e3315b677129b9d97c09b573b2
Nikita <nikitabaranov200021@gmail.com>
2019-12-13 17:56:24 +1000

    Init 4 Hometask, made getList and getDetails

---
+++ WebHometask4/WebHometask4/Controllers/GenreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebHometask4.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebHometask4.Controllers
+{
+    public class GenreController : Controller
+    {
+        FilmingContext _context;
+        public GenreController(FilmingContext context) => _context = context;
+
+        [HttpGet]
+        public IActionResult GenreNames(string? pattern)
+        {
+            //if (pattern == null)
+            //{
+                return View(_context.Genres.ToList());
+            //}
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var obj = _context.Genres.ToList().Find(g => g.Id == id);
+            if (obj != null)
+            {
+                return View(obj);
+            }
+            else return NotFound();
+        }
+    }
+}
---
+++ WebHometask4/WebHometask4/Models/FilmingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebHometask4.Models
+{
+    public class FilmingContext : DbContext
+    {
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        public FilmingContext(DbContextOptions<FilmingContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
